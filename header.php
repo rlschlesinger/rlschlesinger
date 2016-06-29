46a82cc8220a08b8c4e46fbfa3b24dc9cfccce3e
Robby Schlesinger <robbyschlesinger@gmail.com>
2016-06-28 18:23:09 -0700

    added navigation

--- header.php
+++ header.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html>
+<html <?php language_attributes(); ?>>
   <head>
     <meta charset="<?php bloginfo('charset'); ?>">
     <meta name="viewport" content="width=device-width">
@@ -7,4 +7,24 @@
     <?php wp_head(); ?>
   </head>
 
-  <body>
+  <body <?php body_class(); ?>>
+    <div class="container">
+
+      <!--site-header -->
+      <header class="site-header">
+        <h1><a href="<?php echo home_url(); ?>"> <?php bloginfo('name'); ?></a></h1>
+        <h5><?php bloginfo('description'); ?></h5>
+
+        <nav class="site-nav">
+
+          <?php
+
+          $args = array(
+            'theme_location' => 'primary'
+          )
+
+          ?>
+          <?php wp_nav_menu($args); ?>
+        </nav>
+
+      </header>
